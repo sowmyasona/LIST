ebd08ee134bd7547ea4e4de1f8968bd4aadc70cf
sona@1027 <118789307+sowmyasona@users.noreply.github.com>
2022-12-25 21:54:34 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,12 @@
+# list = ['physics', 'chemistry', 1997, 2000];
+# print ("Value available at index 2 : ")
+# print (list[2])
+# list[2] = "liki";
+# print ("New value available at index 2 : ")
+# print (list[2])
+
+list1 = ['physics', 'chemistry', 1997, 'sona',45];
+print (list1)
+del list1[2];
+print ("After deleting value at index 2 : ")
+print (list1)
